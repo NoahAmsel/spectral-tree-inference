3d9045d5c153155ec67028bcbe4208763178d5e5
Noah Amsel <ebminmaj75@gmail.com>
2019-09-22 14:35:18 -0400

    wrote distance functions, tree interface

--- reconstruct_tree.py
+++ reconstruct_tree.py
@@ -2,7 +2,7 @@ from itertools import combinations
 import numpy as np
 import scipy.spatial.distance
 
-def similarity_matrix(observations, classes=None):
+def paralinear_distance(observations, classes=None):
     # build the similarity matrix M -- this is pretty optimized, at least assuming k << n
     # setting classes allows you to ignore missing data. e.g. set class to 1,2,3,4 and it will
     # treat np.nan, -1, and 5 as missing data
@@ -18,27 +18,37 @@ def similarity_matrix(observations, classes=None):
     #M = np.linalg.det(confusion_matrices/sqrt(n))
 
     diag = M.diagonal()
-    return M / np.sqrt(np.outer(diag,diag))
+    similarity = M / np.sqrt(np.outer(diag,diag))
+    return -np.log(np.clip(similarity, a_min=1e-20, a_max=None))
 
-def JC_similarity_matrix(observations, classes=None):
-    # TODO: use classes argument to make this robust to missing data
+def JC_distance_matrix(observations, classes=None):
+    assert classes is None
     if classes is None:
         classes = np.unique(observations)
-    else:
-        assert False
     k = len(classes)
+    hamming_matrix = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(observations, metric='hamming'))
+    inside_log = 1 - hamming_matrix*k/(k-1)
+    inside_log = np.clip(inside_log, a_min=1e-16, a_max=None)
+    return - ((k-1)/k) * np.log(inside_log)
 
-    normalized_hamming_distance = scipy.spatial.distance.pdist(observations, metric='hamming')
-    # under J-C model, even independent sequences have a maximum hamming distance of (k-1)/k
-    # in which case determinant is 0
-    normalized_hamming_distance = np.clip(normalized_hamming_distance, a_min=None, a_max=(k-1)/k)
-    expected_dets = ((1/k)**k)*(1 - normalized_hamming_distance*k/(k-1))**(k-1)
-    #expected_dets = (1/k - normalized_hamming_distance/(k-1))**(k-1) / k  # this is the same
-    dm = scipy.spatial.distance.squareform(expected_dets)
-    # under jukes-cantor model, base frequencies are equal at all nodes
-    # so for diag elements, take determinant of (1/k)* I_k
-    np.fill_diagonal(dm, (1/k)**k)
-    return dm
+def correlation_distance_matrix(observations):
+    corr = np.corrcoef(observations)
+    corr = np.clip(corr, a_min=1e-16, a_max=None)
+    return np.log(corr)
 
-def linear_similarity_matrix(observations):
-    return np.corrcoef(observations)**2
+
+if __name__ == "__main__":
+    from utils import *
+    t = balanced_binary(4)
+    all_data = temp_dataset_maker(t, 1000, 0.1)
+    observations, _ = charmatrix2array(all_data[0])
+    dist = JC_distance_matrix(observations)
+    dist[:5,:5]
+
+    dist = paralinear_distance(observations)
+    dist[:5,:5]
+
+    dist = JC_similarity_matrix(observations)
+    np.log(dist[:5,:5])
+
+    scipy.spatial.distance.pdist(observations, metric='hamming')
--- utils.py
+++ utils.py
@@ -1,9 +1,26 @@
 import dendropy
-import numpy
+import numpy as np
 from dendropy.interop import seqgen
 
 def new_default_namespace(num_taxa):
     return dendropy.TaxonNamespace([str(i) for i in range(num_taxa)])
+
+def leaf(i, namespace, **kwargs):
+    # for now, `i` will specify the taxon but in the future can make it more flexible
+    assert 'taxon' not in kwargs
+    kwargs['taxon'] = namespace[i]
+    node = dendropy.Node(**kwargs)
+    node.taxa_set = np.full(len(namespace), False)
+    node.taxa_set[i] = True
+    return node
+
+def merge_children(children, **kwargs):
+    node = dendropy.Node(**kwargs)
+    for child in children:
+        node.add_child(child)
+    if all(hasattr(child,'taxa_set') for child in children):
+        node.taxa_set = np.logical_or.reduce(tuple(child.taxa_set for child in children))
+    return node
 
 def balanced_binary(depth, namespace=None):
     num_taxa = 2**depth
@@ -12,42 +29,72 @@ def balanced_binary(depth, namespace=None):
     else:
         assert num_taxa == len(namespace)
 
-    nodes = [dendropy.Node(taxon=namespace[i], edge_length=1) for i in range(num_taxa)]
+    nodes = [leaf(i, namespace, edge_length=1) for i in range(num_taxa)]
     while len(nodes) > 1:
-        next_level = []
-        for i in range(len(nodes)//2):
-            node = dendropy.Node(edge_length=1)
-            node.add_child(nodes[2*i])
-            node.add_child(nodes[2*i+1])
-            next_level.append(node)
-        nodes = next_level
+        nodes = [merge_children(nodes[2*i : 2*i+2], edge_length=1) for i in range(len(nodes)//2)]
 
-    return dendropy.Tree(taxon_namespace=namespace, seed_node = nodes[0])
+    return dendropy.Tree(taxon_namespace=namespace, seed_node=nodes[0])
 
-def temp_dataset_maker(tree_list, seq_len):
+def temp_dataset_maker(tree_list, seq_len, scaler):
     s = seqgen.SeqGen()
     #s.model = "GTR"
     s.seq_len = seq_len
     #s.num_partitions = 1 # what does this even do?
+    s.scale_branch_lens = scaler
     d0 = s.generate(tree_list)
     #print(s._compose_arguments())
     return d0.char_matrices
 
+def charmatrix2array(charmatrix):
+    #charmatrix[taxon].values()
+    alphabet = charmatrix.state_alphabets[0]
+    return np.array([[state_id.index for state_id in charmatrix[taxon].values()]for taxon in charmatrix]), alphabet
+
+def array2charmatrix(matrix, alphabet):
+    pass
 
 if __name__ == "__main__":
     t = balanced_binary(4)
     t.print_plot()
+
     trees = dendropy.TreeList()
     trees.extend([t, t])
     trees.taxon_namespace is trees[0].taxon_namespace
     trees.taxon_namespace is trees[-1].taxon_namespace
 
-    all_data = temp_dataset_maker(trees, 25)
+    trees.taxon_namespace[2]
+
+    all_data = temp_dataset_maker(trees, 1000)
     t0_data = all_data[0]
-    type(t0_data)
+    observations, _ = charmatrix2array(t0_data)
 
     """model = dendropy.model.discrete.Jc69() #
     model = dendropy.model.discrete.DiscreteCharacterEvolver()
     mat = dendropy.model.discrete.simulate_discrete_chars(100, t, model)
     hh = mat[0]
     hh.symbols_as_string()"""
+
+    pdm = dendropy.PhylogeneticDistanceMatrix(t)
+    print(pdm)
+    tt = pdm.as_data_table()
+
+
+
+    def similarity_matrix(observations, classes=None):
+        # build the similarity matrix M -- this is pretty optimized, at least assuming k << n
+        # setting classes allows you to ignore missing data. e.g. set class to 1,2,3,4 and it will
+        # treat np.nan, -1, and 5 as missing data
+        if classes is None:
+            classes = np.unique(observations)
+        observations_one_hot = np.array([observations==cls for cls in classes], dtype='int')
+        # this command makes an (m x m) array of (k x k) confusion matrices
+        # where m = number of leaves and k = number of classes
+        confusion_matrices = np.einsum("jik,mlk->iljm", observations_one_hot, observations_one_hot)
+        # valid_observations properly accounts for missing data I think
+        valid_observations = confusion_matrices.sum(axis=(2,3), keepdims=True)
+        M = np.linalg.det(confusion_matrices/valid_observations)     # same as `confusion_matrices/n' when there aren't null entries
+        #M = np.linalg.det(confusion_matrices/sqrt(n))
+        return M
+
+
+    similarity_matrix(observations)
