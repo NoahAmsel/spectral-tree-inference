4f59274ac64096630029712a3b09e7aa5235744c
Noah Amsel <ebminmaj75@gmail.com>
2019-09-28 16:16:44 -0400

    estimate_tree_topology()

--- reconstruct_tree.py
+++ reconstruct_tree.py
@@ -1,6 +1,15 @@
 from itertools import combinations
 import numpy as np
 import scipy.spatial.distance
+import utils
+
+def score_split(A, M):
+    M_A = M[np.ix_(A, ~A)]        # same as M[A,:][:,~A]
+    s = np.linalg.svd(M_A, compute_uv=False)
+    if s.size <= 1:
+        return 0    # automatically rank 1, so we say "the second eigenvalue" is 0
+    else:
+        return s[1] # the second eigenvalue
 
 def paralinear_distance(observations, classes=None):
     # build the similarity matrix M -- this is pretty optimized, at least assuming k << n
@@ -19,7 +28,8 @@ def paralinear_distance(observations, classes=None):
 
     diag = M.diagonal()
     similarity = M / np.sqrt(np.outer(diag,diag))
-    return -np.log(np.clip(similarity, a_min=1e-20, a_max=None))
+    similarity = np.clip(similarity, a_min=1e-20, a_max=None)
+    return -np.log(similarity)
 
 def JC_distance_matrix(observations, classes=None):
     assert classes is None
@@ -29,13 +39,61 @@ def JC_distance_matrix(observations, classes=None):
     hamming_matrix = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(observations, metric='hamming'))
     inside_log = 1 - hamming_matrix*k/(k-1)
     inside_log = np.clip(inside_log, a_min=1e-16, a_max=None)
-    return - ((k-1)/k) * np.log(inside_log)
+    return - (k-1) * np.log(inside_log)
 
 def correlation_distance_matrix(observations):
     corr = np.corrcoef(observations)
     corr = np.clip(corr, a_min=1e-16, a_max=None)
     return np.log(corr)
 
+def estimate_tree_topology(distance_matrix, namespace=None, scorer=score_split, bifurcating=False):
+    m, m2 = distance_matrix.shape
+    assert m == m2, "Distance matrix must be square"
+    if namespace is None:
+        namespace = utils.new_default_namespace(m)
+    else:
+        assert len(namespace) >= m, "Namespace too small for distance matrix"
+
+    # initialize leaf nodes
+    G = [utils.leaf(i, namespace) for i in range(m)]
+
+    available_clades = set(range(len(G)))   # len(G) == m
+    # initialize Sigma
+    Sigma = np.full((2*m,2*m), np.nan)  # we should only use entries that we set later; init to nan so they'll throw an error if we do
+    sv1 = np.full((2*m,2*m), np.nan)
+    sv2 = np.full((2*m,2*m), np.nan)
+    for i,j in combinations(available_clades, 2):
+        A = G[i].taxa_set | G[j].taxa_set
+        Sigma[i,j] = scorer(A, M)
+        Sigma[j,i] = Sigma[i,j]    # necessary b/c sets have unstable order, so `combinations' could return either one
+        #sv1[i,j] = first_sv(A, M)
+        #sv1[j,i] = sv1[i,j]
+        #sv2[i,j] = second_sv(A, M)
+        #sv2[j,i] = sv2[i,j]
+
+    # merge
+    while len(available_clades) > (2 if bifurcating else 3): # this used to be 1
+        left, right = min(combinations(available_clades, 2), key=lambda pair: Sigma[pair])
+        G.append(utils.merge_children((G[left], G[right])))
+        new_ix = len(G) - 1
+        available_clades.remove(left)
+        available_clades.remove(right)
+        for other_ix in available_clades:
+            A = G[other_ix].taxa_set | G[-1].taxa_set
+            Sigma[other_ix, new_ix] = scorer(A, M)
+            Sigma[new_ix, other_ix] = Sigma[other_ix, new_ix]    # necessary b/c sets have unstable order, so `combinations' could return either one
+        available_clades.add(new_ix)
+
+    # HEYYYY why does ariel do something special for the last THREE groups? (rather than last two)
+    # think about what would happen if at the end we have three groups: 1 leaf, 1 leaf, n-2 leaves
+    # how would we know which leaf to attach, since score would be 0 for both??
+
+    # return Phylo.BaseTree.Tree(G[-1])
+
+    # for a bifurcating tree we're combining the last two available clades
+    # for an unrooted one it's the last three because
+    # if we're making unrooted comparisons it doesn't really matter which order we attach the last three
+    return dendropy.Tree(taxon_namespace=namespace, seed_node=utils.merge_children((G[i] for i in available_clades)))
 
 if __name__ == "__main__":
     from utils import *
---
+++ trials.py
@@ -0,0 +1,139 @@
+# %%
+import numpy as np
+
+import reconstruct_tree
+
+def grow_tree_recover(m, n, k, proba_bounds, verbose=True):
+    reference_tree = random_discrete_tree(m, n, k, proba_bounds=proba_bounds)
+    if verbose:
+        reference_tree.root.ascii()
+    observations, labels = reference_tree.root.observe()
+    inferred_tree = estimate_tree_topology_multiclass(observations, labels=labels)
+    if verbose:
+        Phylo.draw_ascii(inferred_tree)
+    NoahClade.tree_Fscore(inferred_tree, reference_tree)
+    print(NoahClade.equal_topology(inferred_tree, reference_tree))
+
+def load_tree_recover(reference_tree, n, k=None, proba_bounds=(0.50, 0.95), verbose=True, format='newick'):
+    # if tree is a file, load the tree it contains
+    # if tree is a tree object, generate data, possibly using preexisting transition matrices
+    if isinstance(reference_tree, str):
+        reference_tree = Phylo.read(reference_tree, format)
+        reference_tree.root = NoahClade.NoahClade.convertClade(reference_tree.root)
+    if hasattr(reference_tree.root, 'transition') and hasattr(reference_tree.root.transition, 'matrix'):
+        k = reference_tree.root.transition.shape[1]
+    root_data = np.random.choice(a=k, size=n)
+    transition_maker = NoahClade.NoahClade.gen_symmetric_transition
+    reference_tree.root.gen_subtree_data(root_data, transition_maker, num_classes=k, proba_bounds=proba_bounds)
+
+    if verbose:
+        reference_tree.root.ascii()
+    observations, labels = reference_tree.root.observe()
+    reference_tree.root.reset_taxasets(labels=labels)
+    inferred_tree = estimate_tree_topology_multiclass(observations, labels=labels)
+    inferred_tree.root.reset_taxasets(labels=labels)
+    if verbose:
+        Phylo.draw_ascii(inferred_tree)
+    NoahClade.tree_Fscore(inferred_tree, reference_tree)
+    NoahClade.equal_topology(inferred_tree, reference_tree)
+
+def even_branch_lengths(tree):
+    for node in tree.find_elements():
+        node.branch_length = 1
+
+def load_observations_recover(reference_tree_path=None, observation_matrix_path=None, comparison_tree_path=None, verbose=False, format='newick', method=estimate_tree_topology_multiclass):
+    reference_tree = Phylo.read(reference_tree_path, format)
+    reference_tree.root = NoahClade.NoahClade.convertClade(reference_tree.root)
+    if verbose:
+        print("="*40)
+        print("REFERENCE")
+        even_branch_lengths(reference_tree)
+        reference_tree.root.ascii()
+
+    observations = np.genfromtxt(observation_matrix_path, delimiter=",")
+    assert observations.shape[0] == reference_tree.count_terminals() # one row per leaf
+    labels = ["_"+str(i) if i < 10 else str(i) for i in range(1, observations.shape[0]+1)]
+    inferred_tree = method(observations, labels=labels)
+    reference_tree.root.reset_taxasets(labels=labels)
+    inferred_tree.root.reset_taxasets(labels=labels)
+    if verbose:
+        print("INFERRED")
+        even_branch_lengths(inferred_tree)
+        inferred_tree.root.ascii()
+
+    print("=== Compare to gold standard ===")
+    F1_gold, _, _, _ = NoahClade.tree_Fscore(inferred_tree, reference_tree)
+    print(NoahClade.equal_topology(inferred_tree, reference_tree))
+    print(F1_gold)
+
+    if comparison_tree_path is not None:
+        external_inferred = Phylo.read(comparison_tree_path, format)
+        external_inferred.root = NoahClade.NoahClade.convertClade(external_inferred.root)
+        external_inferred.root.reset_taxasets(labels=labels)
+        print("=== Compare to Matlab version ===")
+        F1_matlab, _, _, _ = NoahClade.tree_Fscore(inferred_tree, external_inferred)
+        print(NoahClade.equal_topology(inferred_tree, external_inferred))
+        print(F1_matlab)
+        if verbose:
+            print("MATLAB")
+            even_branch_lengths(external_inferred)
+            external_inferred.root.ascii()
+
+def main():
+    #cProfile.run('generate_and_recover(m=64, n=10**4, k=4, proba_bounds=(0.8, 0.95))')
+    grow_tree_recover(m=64, n=10**4, k=4, proba_bounds=(0.80, 0.95))
+    #cProfile.run('generate_and_recover(m=200, n=4000, k=4, proba_bounds=(0.8, 0.95))')
+    #generate_and_recover(m=200, n=4000, k=4, proba_bounds=(0.8, 0.95))
+
+def main2():
+    path = '/Users/noah/Dropbox/reconstructing_trees/code/output_trees/phy_mc_ref.tree'
+    load_tree_recover(reference_tree=path, n=10**4, k=4, proba_bounds=(0.80, 0.95))
+
+def main3():
+    load_observations_recover(reference_tree_path='/Users/noah/Dropbox/reconstructing_trees/code/output_trees/phy_mc_ref.tree',
+                observation_matrix_path='/Users/noah/Dropbox/reconstructing_trees/code/observations.txt',
+                comparison_tree_path='/Users/noah/Dropbox/reconstructing_trees/code/output_trees/spec_mc.tree', verbose=True)
+# %%
+
+if __name__ == "__main__":
+    main()
+    main2()
+    main3()
+
+    from scipy.spatial.distance import squareform, pdist
+    from baselines import distance_correction
+
+    load_observations_recover(reference_tree_path="./NJ_ref.tree",
+                                observation_matrix_path="obs.csv",
+                                comparison_tree_path="NJ_test.tree", method=NJ_JC, verbose=True)
+
+    dists = np.genfromtxt("dists.csv", delimiter=",")
+    matlab_dm = squareform(dists)
+
+    obs = np.genfromtxt("obs.csv", delimiter=",")
+    classes = np.unique(obs)
+    k = len(classes)
+    hamming = squareform(pdist(obs, metric='hamming'))
+    dm = distance_correction(hamming, k)
+
+    dm[dm > 5]
+
+
+    pdist(obs[], metric='hamming')
+
+    pdist(obs[(0,5),:], metric='hamming')
+
+    dm[0,:6]
+    matlab_dm[0,:6]
+    np.max(dm)
+    np.max(matlab_dm)
+    np.max(np.abs(matlab_dm - dm))
+    np.argmax(dm - matlab_dm)
+    dm[0,4]
+    matlab_dm[0,4]
+
+    np.allclose(dm, matlab_dm, atol=0.01)
+
+    load_observations_recover(reference_tree_path="./NJ_ref.tree",
+                                observation_matrix_path="obs.csv",
+                                comparison_tree_path="NJ_test.tree", method=NJ_hamming, verbose=True)
