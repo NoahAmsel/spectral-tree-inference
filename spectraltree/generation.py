e744be6a67f9a8ade483c98d9b44338fa87f437f
Yariv A <aizeny@gmail.com>
2020-01-31 10:41:23 -0500

    test commiting to new branch

--- spectraltree/generation.py
+++ spectraltree/generation.py
@@ -146,29 +146,6 @@ class GTR(ContinuousTimeDiscreteTransition):
         super().__init__(stationary_freqs, Q)
         self._transition_rates = transition_rates
 
-class TN93(GTR):
-    def __init__(self, stationary_freqs, kappa1, kappa2):
-        #  we could allow for more than for classes
-        # assume the first half are of one type ("pyramidines") and the second half is of the other type ("purines")
-        # but right now we don't
-        assert len(stationary_freqs) == 4
-        transition_rates = np.array([kappa1, 1, 1, 1, 1, kappa2]).astype(float)
-        super().__init__(stationary_freqs, transition_rates)
-
-class T92(TN93):
-    def __init__(self, theta, kappa1, kappa2):
-        #  we could allow for more than for classes
-        # assume the first half are of one type ("pyramidines") and the second half is of the other type ("purines")
-        # but right now we don't
-        assert 0. <= theta <= 1.
-        GC = theta / 2.
-        AT = (1. - theta) / 2.
-        super().__init__(np.array([AT, GC, GC, AT]), kappa1, kappa2)
-
-class HKY(TN93):
-    def __init__(self, stationary_freqs, kappa):
-        super().__init__(stationary_freqs, kappa, kappa)
-
 class Jukes_Cantor(GTR):
     def __init__(self, num_classes=4):
         base_frequencies = np.ones(num_classes)
@@ -201,18 +178,73 @@ class Jukes_Cantor(GTR):
         suff = "" if self.k==4 else " (k={})".format(self.k)
         return "Jukes Cantor" + suff
 
+def numpy_matrix_with_characters_on_tree_ordered(seq_attr, tree):
+    """
+    Extracts sequences from all leaves and packs them into a numpy matrix.
+    Repalces `extend_char_matrix_with_characters_on_tree` method of `DiscreteCharacterEvolver`, which doesn't use numpy.
+    """
+    sequences = [0 for i in tree.taxon_namespace]
+    for leaf_ix, leaf in enumerate(tree.leaf_node_iter()):
+        
+        sequences[tree.taxon_namespace[:].index(leaf.taxon)] = getattr(leaf, seq_attr)[-1]
+        #sequences.append(np.concatenate(getattr(leaf, seq_attr)))
+    return np.array(sequences)
+    
 def numpy_matrix_with_characters_on_tree(seq_attr, tree):
     """
     Extracts sequences from all leaves and packs them into a numpy matrix.
     Repalces `extend_char_matrix_with_characters_on_tree` method of `DiscreteCharacterEvolver`, which doesn't use numpy.
     """
-    index_map = {taxon: ix for ix, taxon in enumerate(tree.taxon_namespace)}
-    sequences = [None]*len(tree.leaf_nodes())
-    assert len(sequences) == len(index_map)  # exactly one entry in taxon_namespace for each leaf
-    for leaf in tree.leaf_node_iter():
-        sequences[index_map[leaf.taxon]] = getattr(leaf, seq_attr)[-1]
-        #sequences[index_map(leaf.taxon)] = np.concatenate(getattr(leaf, seq_attr))
+    sequences = []
+    for leaf_ix, leaf in enumerate(tree.leaf_node_iter()):
+        sequences.append(getattr(leaf, seq_attr)[-1])
+        #sequences.append(np.concatenate(getattr(leaf, seq_attr)))
     return np.array(sequences)
+
+def simulate_sequences_ordered(seq_len, tree_model, seq_model, mutation_rate=1.0, root_states=None, retain_sequences_on_tree=False, rng=None):
+    """
+    Convenience function that generates a matrix of sequence observations from a given sequence model and tree
+
+    Parameters
+    ----------
+
+    seq_len       : int
+        Length of sequence (number of characters).
+    tree_model    : |Tree|
+        Tree on which to simulate.
+    seq_model     : |Transition|
+        The character substitution model under which to to evolve the
+        characters.
+    mutation_rate : float
+        Mutation *modifier* rate (should be 1.0 if branch lengths on tree
+        reflect true expected number of changes).
+    root_states   : list
+        Vector of root states (length must equal ``seq_len``).
+    retain_sequences_on_tree : bool
+        If |False|, sequence annotations will be cleared from tree after
+        simulation. Set to |True| if you want to, e.g., evolve and accumulate
+        different sequences on tree, or retain information for other purposes.
+    rng           : random number generator
+        If not given, 'GLOBAL_RNG' will be used.
+
+    Returns
+    -------
+
+    char_matrix :  |numpy.array|
+        Matrix where each row is the sequence generated for a given leaf.
+
+    """
+    seq_evolver = dendropy.model.discrete.DiscreteCharacterEvolver(seq_model=seq_model, mutation_rate=mutation_rate)
+    tree = seq_evolver.evolve_states(
+        tree=tree_model,
+        seq_len=seq_len,
+        root_states=root_states,
+        rng=rng)
+    char_matrix = numpy_matrix_with_characters_on_tree_ordered(seq_evolver.seq_attr, tree_model)
+    if not retain_sequences_on_tree:
+        seq_evolver.clean_tree(tree)
+    return char_matrix
+
 
 def simulate_sequences(seq_len, tree_model, seq_model, mutation_rate=1.0, root_states=None, retain_sequences_on_tree=False, rng=None):
     """
