05b49815d4e10fdd9dfe385681b37515a1d5ff85
Yariv A <aizeny@gmail.com>
2020-02-03 23:44:03 -0500

    Fixing raxml for windows

--- tests/Raxml_test.py
+++ tests/Raxml_test.py
@@ -10,23 +10,36 @@ Make sure to include `raxml_args=["-T 2"]`
 On other operating systems you may need to compile yourself using the directions here
 https://cme.h-its.org/exelixis/web/software/raxml/hands_on.html
 """
-import sys, os
+import sys, os, platform
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'spectraltree'))
-
-from utils import balanced_binary
-from generation import Jukes_Cantor
+import reconstruct_tree
+import utils 
+import generation
 from dendropy.interop import raxml
 from dendropy.model.discrete import simulate_discrete_chars, Jc69
 from dendropy.calculate.treecompare import symmetric_difference
 
-reference_tree = balanced_binary(128)
+reference_tree = utils.balanced_binary(16)
 reference_tree.print_plot()
 
-data = simulate_discrete_chars(1000, reference_tree, Jc69(), mutation_rate=Jukes_Cantor().p2t(0.95), )
+data = simulate_discrete_chars(1000, reference_tree, Jc69(), mutation_rate=generation.Jukes_Cantor().p2t(0.95), )
 
-rx = raxml.RaxmlRunner(raxml_path = os.path.join(os.path.dirname(sys.path[0]),'spectraltree'))
+if platform.system() == 'Windows':
+    # Windows version:
+    rx = raxml.RaxmlRunner(raxml_path = os.path.join(os.path.dirname(sys.path[0]),r'spectraltree\raxmlHPC-SSE3.exe'))
+elif platform.system() == 'Darwin':
+    #MacOS version:
+    rx = raxml.RaxmlRunner()
+
 tree = rx.estimate_tree(char_matrix=data, raxml_args=["-T 2"])
+
+
 
 tree.print_plot()
 
-print(symmetric_difference(reference_tree, tree))
+print("symmetric_difference: ",symmetric_difference(reference_tree, tree))
+RF,F1 = reconstruct_tree.compare_trees(reference_tree, tree)
+print("raxml: ")
+print("RF = ",RF)
+print("F1% = ",F1)
+print("")
