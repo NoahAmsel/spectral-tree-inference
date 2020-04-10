0873503ba3e08fd774099c3972c18217be77e5ee
Mamie Wang <mamie.wang@yale.edu>
2020-04-10 15:03:00 -0400

    Added HKY similarity matrix function

--- spectraltree/reconstruct_tree.py
+++ spectraltree/reconstruct_tree.py
@@ -78,6 +78,53 @@ def estimate_edge_lengths(tree, distance_matrix):
     # check the PAUP* documentation
     pass
 
+def HKY_similarity_matrix(observations, classes=None, verbose = False):
+    m, N = observations.shape
+    if classes is None:
+        classes = np.unique(observations)
+    k = len(classes)
+    # From Tamura, K., and M. Nei. 1993
+    # for each pair of sequences, 
+    # 1. estimate the average base frequency for pairs of sequences
+    # 2. compute purine transition proportion P1 (A <-> G)
+    # 3. compute pyrimidine transition proportion P2 (T <-> C)
+    # 3. compute transversion proportion Q (A <-> C, A <-> T, G <-> C, G <-> T)
+
+    if verbose: print("Computing the average base frequency for each pair of sequences...")
+    g = {}
+    for x in classes:
+        obs_x = observations == x
+        g[x] = np.array([np.mean(np.hstack([a, b])) for a, b in product(obs_x, repeat = 2)]).reshape((m, m))
+    
+    g["R"] = g["A"] + g["G"]
+    g["Y"] = g["T"] + g["C"]
+    
+    # compute transition and transversion proportion
+    if verbose: print("Computing transition and transversion proportion for each pair of sequences...")
+    P = {}
+    for i, x in enumerate(classes):
+        other_classes = np.delete(classes, i)
+        for y in other_classes:
+            P_x_y = np.array([np.mean(np.logical_and(a == x, b == y)) for a, b in product(observations, repeat = 2)]).reshape((m, m))
+            P[x + y] = P_x_y
+            
+    P_1 = P['AG'] + P["GA"]
+    P_2 = P['CT'] + P['TC']
+    Q = P['AC'] + P['CA'] + P['AT'] + P['TA'] +\
+        P['GC'] + P['CG'] + P['GT'] + P['TG']
+
+    # compute the similarity (formula 7)
+    if verbose: print("Computing similarity matrix")
+    R = (1 - g["R"]/(2 * g["A"] * g["G"]) * P_1 - 1 / (2 * g["R"]) * Q)
+    Y = (1 - g["Y"]/(2 * g["T"] * g["C"]) * P_2 - 1 / (2 * g["Y"]) * Q)
+    T = (1 - 1/(2 * g["R"] * g["Y"]) * Q)
+    S = np.sign(R) * (np.abs(R))**(2 * g["A"] * g["G"] / g["R"])
+    S += np.sign(Y) * (np.abs(Y))**(2 * g["T"] * g["C"] / g["Y"])
+    S += np.sign(T) * (np.abs(T))**(2 * (g["R"] * g["Y"] - g["A"] * g["G"] * g["Y"] / g["R"] - g["T"] * g["C"] * g["R"] / g["Y"]))
+
+    return S
+
+
 ##########################################################
 ##               Reconstruction methods
 ##########################################################
