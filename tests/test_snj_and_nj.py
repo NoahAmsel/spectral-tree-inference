f120d3707f689b8d2d980481e5781b5d30a336fc
Yariv A <aizeny@gmail.com>
2020-05-28 16:37:58 -0400

    Corrected generation methods

--- tests/test_snj_and_nj.py
+++ tests/test_snj_and_nj.py
@@ -25,32 +25,28 @@ from character_matrix import FastCharacterMatrix
 from dendropy.model.discrete import simulate_discrete_chars, Jc69, Hky85
 from dendropy.calculate.treecompare import symmetric_difference
 
-N = 1000
+N = 5000
 num_taxa = 32
 
 #################################
 ## Tree generation
 #################################
-
+print("Creating tree")
 jc = generation.Jukes_Cantor()
 # hky = generation.HKY(kappa = 2)
-mutation_rate = [jc.p2t(0.95)]
+#mutation_rate = [jc.p2t(0.95)]
+mutation_rate = [0.1]
 
-reference_tree = utils.unrooted_birth_death_tree(num_taxa, birth_rate=0.5)
-# reference_tree = utils.lopsided_tree(num_taxa)
-#reference_tree = utils.balanced_binary(num_taxa)
-#for x in reference_tree.preorder_edge_iter():
-#    x.length = 1
+#reference_tree = utils.unrooted_birth_death_tree(num_taxa, birth_rate=0.5)
+reference_tree = utils.lopsided_tree(num_taxa)
+# reference_tree = utils.balanced_binary(num_taxa)
+# for x in reference_tree.preorder_edge_iter():
+#     x.length = 0.5
 print("Genration observations by JC and HKY")
-observationsJC = FastCharacterMatrix(generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=jc, mutation_rate=mutation_rate))
-observationsHKY = FastCharacterMatrix(simulate_discrete_chars(N, reference_tree, Hky85(kappa = 2), mutation_rate=mutation_rate[0]))
-# observationsHKY = list()
-# for t in observationsHKY_d.taxon_namespace:
-#     observationsHKY.append([x.symbol for x in observationsHKY_d[t]])
-# observationsHKY = np.array(observationsHKY)
+#observationsJC = FastCharacterMatrix(generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=jc, mutation_rate=mutation_rate))
+observationsJC = FastCharacterMatrix(simulate_discrete_chars(N, reference_tree, Jc69(), mutation_rate=mutation_rate[0]))
+observationsHKY = FastCharacterMatrix(simulate_discrete_chars(N, reference_tree, Hky85(kappa = 1), mutation_rate=mutation_rate[0]))
 
-
-# observationsHKY = generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=hky, mutation_rate=mutation_rate)
 #################################
 ## SNJ - Jukes_Cantor
 #################################
