caaf3ce6280122a61f4f9392493eb1dd0df1bff8
Yariv A <aizeny@gmail.com>
2020-06-26 13:39:58 -0400

    changing the STR test

--- tests/test_spectral_tree_reonstruction.py
+++ tests/test_spectral_tree_reonstruction.py
@@ -22,19 +22,31 @@ import reconstruct_tree
 from dendropy.model.discrete import simulate_discrete_chars, Jc69
 from dendropy.calculate.treecompare import symmetric_difference
 
-N = 400
+N = 300
 num_taxa = 32
 jc = generation.Jukes_Cantor()
 mutation_rate = [jc.p2t(0.95)]
-
-reference_tree = utils.unrooted_birth_death_tree(num_taxa, birth_rate=1)
+np.random.seed(0)
+# reference_tree = utils.unrooted_birth_death_tree(num_taxa, birth_rate=1)
+# reference_tree = utils.lopsided_tree(num_taxa)
+reference_tree = utils.balanced_binary(num_taxa)
 for x in reference_tree.preorder_edge_iter():
     x.length = 1
-observations = generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=jc, mutation_rate=mutation_rate)
-spectral_method = reconstruct_tree.SpectralTreeReconstruction(reconstruct_tree.RAxML,reconstruct_tree.JC_similarity_matrix)   
-tree_rec = spectral_method(observations,reference_tree.taxon_namespace)
+np.random.seed(0)
+t0 = time.time()
+observations = generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=jc, mutation_rate=mutation_rate,rng=np.random)
+print("gen time: ", time.time() - t0)
+spectral_method = reconstruct_tree.SpectralTreeReconstruction(reconstruct_tree.NeighborJoining,reconstruct_tree.JC_similarity_matrix)   
+
+t0 = time.time()
+tree_rec = spectral_method.deep_spectral_tree_reconstruction(observations, reconstruct_tree.JC_similarity_matrix,
+                                    taxon_namespace = reference_tree.taxon_namespace,
+                                    threshhold = 4,verbose=True)
+t = time.time() - t0
+
 RF,F1 = reconstruct_tree.compare_trees(tree_rec, reference_tree)
 print("Spectral: ")
+print("time = ", t)
 print("RF = ",RF)
 print("F1% = ",F1)
 print("")
