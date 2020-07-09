18eea5dea566fc7ee4684e2190777bb2ba49a17d
Yariv A <aizeny@gmail.com>
2020-07-09 15:43:42 -0400

    imporivng test

--- tests/test_deep_spectral_tree_reonstruction.py
+++ tests/test_deep_spectral_tree_reonstruction.py
@@ -22,33 +22,40 @@ import reconstruct_tree
 from dendropy.model.discrete import simulate_discrete_chars, Jc69
 from dendropy.calculate.treecompare import symmetric_difference
 
-N = 500
-num_taxa = 64
+N = 1000
+num_taxa = 128
 jc = generation.Jukes_Cantor()
 mutation_rate = [jc.p2t(0.95)]
-num_itr = 20
+num_itr = 1
 # reference_tree = utils.unrooted_birth_death_tree(num_taxa, birth_rate=1)
 # for x in reference_tree.preorder_edge_iter():
 #     x.length = 1
-merging_method_list = ['least_square','angle']
-RF = {'least_square': [], 'angle': []}
-F1 = {'least_square': [], 'angle': []}
-for merge_method in merging_method_list:
-    for i in range(num_itr):
-        #reference_tree = utils.balanced_binary(num_taxa)
-        reference_tree = utils.lopsided_tree(num_taxa)
-        observations = generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=jc, mutation_rate=mutation_rate)
+merging_method_list = ['angle']
+#merging_method_list = []
+RF = {'least_square': [], 'angle': [], 'tensor': []}
+F1 = {'least_square': [], 'angle': [], 'tensor': []}
+for i in range(num_itr):
+    #reference_tree = utils.balanced_binary(num_taxa)
+    reference_tree = utils.lopsided_tree(num_taxa)
+    observations = generation.simulate_sequences_ordered(N, tree_model=reference_tree, seq_model=jc, mutation_rate=mutation_rate)        
+    for merge_method in merging_method_list:
+    
         spectral_method = reconstruct_tree.SpectralTreeReconstruction(reconstruct_tree.NeighborJoining,reconstruct_tree.JC_similarity_matrix)   
+        time_s = time.time()
+        Raxml  = reconstruct_tree.RAxML()
         tree_rec = spectral_method.deep_spectral_tree_reonstruction(observations, reconstruct_tree.JC_similarity_matrix, 
-            taxon_namespace = reference_tree.taxon_namespace, threshhold = 16,merge_method = merge_method)
+            taxon_namespace = reference_tree.taxon_namespace, threshhold = 130,merge_method = merge_method)
+        runtime = time.time()-time_s    
         RF_i,F1_i = reconstruct_tree.compare_trees(tree_rec, reference_tree)
         RF[merge_method].append(RF_i)
         F1[merge_method].append(F1_i)
         
 
 
-print("Angle RF: ",np.mean(RF['angle']))
-print("LS: ",np.mean(RF['least_square']))
+print("Angle RF: ",np.mean(RF['angle']), "Runtime: ", runtime)
+
+#print("LS: ",np.mean(RF['least_square']))
+#print("tensor: ",np.mean(RF['tensor']))
 print("")
 
 
