442e4becfa27e0fb8638b94d1f48e7a589887c9d
Yariv A <aizeny@gmail.com>
2020-07-27 14:04:04 -0400

    Added TreeSVD method, fixed bug in __repr__ funcs

--- spectraltree/reconstruct_tree.py
+++ spectraltree/reconstruct_tree.py
@@ -12,6 +12,10 @@ import dendropy     #should this library be independent of dendropy? is that eve
 from dendropy.interop import raxml
 import utils
 import time
+import os
+import psutil
+from numba import jit
+from sklearn.utils.extmath import randomized_svd
 
 RECONSTRUCT_TREE_PATH = os.path.abspath(__file__)
 RECONSTRUCT_TREE__DIR_PATH = os.path.dirname(RECONSTRUCT_TREE_PATH)
@@ -308,18 +312,21 @@ def compute_alpha_tensor(S_11,S_12,u_12,v_12,bool_array,sigma):
     alpha_square = np.linalg.lstsq( (U_T*sigma)**2,S_T)
     return alpha_square[0]
 
+#@jit(nopython=True)
 def compute_merge_score(mask1A, mask1B, mask2, similarity_matrix, u_12,sigma_12, v_12, O, merge_method= 'angle'):
     
     mask1 = np.logical_or(mask1A, mask1B)
 
     # submatrix of similarities betweeb potential subgroups of 1:
-    S_11_AB = similarity_matrix[np.ix_(mask1A, mask1B)]
+    #S_11_AB = similarity_matrix[np.ix_(mask1A, mask1B)]
+    S_11_AB = similarity_matrix[mask1A, :]
+    S_11_AB = S_11_AB[:, mask1B]
 
     #submatrix of outer product
     # bool_array is indexer into O, which is size (# nodes in T1) x (# nodes in T1)
     bool_array = mask1A[mask1]
-    if sum(bool_array) == len(bool_array):
-        return float("inf")
+    if np.sum(bool_array) == len(bool_array):
+        return np.inf
     O_AB = O[bool_array,:]
     O_AB = O_AB[:,~bool_array]
 
@@ -347,12 +354,12 @@ def compute_merge_score(mask1A, mask1B, mask2, similarity_matrix, u_12,sigma_12,
         O_AB_n = O_AB/np.linalg.norm(O_AB)
         S_11_AB_n = S_11_AB/np.linalg.norm(S_11_AB)        
         score = 1-np.matmul(S_11_AB_n.T,O_AB_n)
-    if merge_method=='tensor':
-        # merge method 2 is tensor method
-        S_11 = S[np.ix_(mask1, mask1)]
-        S_12 = S[np.ix_(mask1, mask2)]
-        alpha_square = compute_alpha_tensor(S_11,S_12,u_12,v_12,bool_array,sigma_12)
-        score = np.linalg.norm(S_11_AB-alpha_square*O_AB)/np.linalg.norm(S_11_AB)
+    # if merge_method=='tensor':
+    #     # merge method 2 is tensor method
+    #     S_11 = S[np.ix_(mask1, mask1)]
+    #     S_12 = S[np.ix_(mask1, mask2)]
+    #     alpha_square = compute_alpha_tensor(S_11,S_12,u_12,v_12,bool_array,sigma_12)
+    #     score = np.linalg.norm(S_11_AB-alpha_square*O_AB)/np.linalg.norm(S_11_AB)
     else:
         Exception("Illigal method: choose least_square, normalized_least_square, angle or tensor")
     return score
@@ -541,7 +548,6 @@ class RAxML(ReconstructionMethod):
         else:
             data = sequences
             
-
         if platform.system() == 'Windows':
             # Windows version:
             rx = raxml.RaxmlRunner(raxml_path = os.path.join(RECONSTRUCT_TREE__DIR_PATH, 'raxmlHPC-SSE3.exe'))
@@ -557,8 +563,105 @@ class RAxML(ReconstructionMethod):
         if taxa_metadata != None:
             tree.taxon_namespace = taxa_metadata.taxon_namespace
         return tree
-    def __repr__():
+    def __repr__(self):
         return "RAxML"
+
+class TreeSVD(ReconstructionMethod):
+    
+    def __call__(self, observations, taxa_metadata=None):        
+        return self.estimate_tree_topology(observations, taxa_metadata)
+
+    #def reconstruct_from_similarity(self, similarity_matrix, taxa_metadata=None):
+    #    return self.estimate_tree_topology(similarity_matrix, taxa_metadata)
+    #change to spectral_neighbor_joining
+
+    def compute_cnt_matrix(self,data,d):
+        m = data.shape[0]
+        #comb_arr = np.array(list(product(state_vec, repeat = 4)))
+        cnt_mtx = np.zeros(d**m)
+        pow_vec = d**(np.arange(m-1,-1,-1))
+        for i in data.T:
+            loc = np.sum(pow_vec*i)
+                #loc = (4**3)*i[0]+(4**2)*i[1]+(4**1)*i[2]+(4**0)*i[3]            
+            cnt_mtx[loc] = cnt_mtx[loc]+1
+        return cnt_mtx
+
+    def compute_flattening_score(self,cnt_mtx,partition,comb_arr,d):
+        # input: 
+        num_taxa = self.observations.shape[0]
+        row_ind = np.where(np.sum(comb_arr[:,partition],axis=1)==0)[0]
+        skip_ind = np.where(np.diff(row_ind)>1)[0]
+        if  len(skip_ind)==0:
+            skip = d**(num_taxa-len(partition))
+        else:
+            skip = skip_ind[0]+1
+    
+        # create flattening matrix
+        flattened_mtx = np.zeros((d**len(partition),d**(num_taxa-len(partition))))
+        for i in np.arange(d**len(partition)):
+            flattened_mtx[i,:] = cnt_mtx[skip*i + row_ind]
+
+        # compute score
+        U,sig,V = randomized_svd(flattened_mtx,n_components=d) 
+        #score = (np.linalg.norm(sig)**2)/(np.linalg.norm(flattened_mtx)**2)
+        score = (np.linalg.norm(flattened_mtx)**2) - (np.linalg.norm(sig)**2)
+        return score
+
+    def estimate_tree_topology(self, observations, taxa_metadata=None,bifurcating=False):
+        self.observations = observations
+        d = len(np.unique(observations))
+        m = observations.shape[0]
+        
+        if taxa_metadata is None:
+            taxa_metadata = utils.TaxaMetadata.default(m)
+        else:
+            assert len(taxa_metadata) >= m, "Namespace too small for distance matrix"
+        
+        # initialize leaf nodes
+        G = taxa_metadata.all_leaves()
+
+        available_clades = set(range(len(G)))   # len(G) == m
+
+        # compute count matrix
+        comb_arr = np.array(list(product(np.arange(d), repeat = m)))
+        cnt_mtx = self.compute_cnt_matrix(observations,d)
+        
+
+        # initialize Sigma
+        Score = np.full((2*m,2*m), np.nan)  # we should only use entries that we set later; init to nan so they'll throw an error if we do
+        #sv1 = np.full((2*m,2*m), np.nan)
+        for i,j in combinations(available_clades, 2):
+            Score[i,j] = self.compute_flattening_score(cnt_mtx,[i,j],comb_arr,d)                       
+            Score[j,i] = Score[i,j]    # necessary b/c sets have unstable order, so `combinations' could return either one
+
+        # merge
+        while len(available_clades) > (2 if bifurcating else 3): # this used to be 1
+            left, right = min(combinations(available_clades, 2), key=lambda pair: Score[pair])
+            G.append(utils.merge_children((G[left], G[right])))
+            new_ix = len(G) - 1
+            available_clades.remove(left)
+            available_clades.remove(right)
+            for other_ix in available_clades:
+                partition = np.where(np.logical_or(G[new_ix].mask,G[other_ix].mask))[0]
+                Score[other_ix, new_ix] = self.compute_flattening_score(cnt_mtx,partition,comb_arr,d)
+                #Sigma[other_ix, new_ix] = scorer(G[other_ix].mask, G[new_ix].mask, similarity_matrix)
+                Score[new_ix, other_ix] = Score[other_ix, new_ix]    # necessary b/c sets have unstable order, so `combinations' could return either one
+            available_clades.add(new_ix)
+
+        # HEYYYY why does ariel do something special for the last THREE groups? (rather than last two)
+        # think about what would happen if at the end we have three groups: 1 leaf, 1 leaf, n-2 leaves
+        # how would we know which leaf to attach, since score would be 0 for both??
+
+        # return Phylo.BaseTree.Tree(G[-1])
+
+        # for a bifurcating tree we're combining the last two available clades
+        # for an unrooted one it's the last three because
+        # if we're making unrooted comparisons it doesn't really matter which order we attach the last three
+        return dendropy.Tree(taxon_namespace=taxa_metadata.taxon_namespace, seed_node=utils.merge_children((G[i] for i in available_clades)), is_rooted=False)
+    def __repr__(self):
+        return "TreeSVD"
+
+
 
 class DistanceReconstructionMethod(ReconstructionMethod):
     def __init__(self, similarity_metric):
@@ -581,7 +684,7 @@ class NeighborJoining(DistanceReconstructionMethod):
         distance_matrix = -np.log(similarity_matrix)
         T = utils.array2distance_matrix(distance_matrix, taxa_metadata).nj_tree()
         return T
-    def __repr__():
+    def __repr__(self):
         return "NJ"
         
 class SpectralNeighborJoining(DistanceReconstructionMethod):
@@ -630,8 +733,9 @@ class SpectralNeighborJoining(DistanceReconstructionMethod):
         # for an unrooted one it's the last three because
         # if we're making unrooted comparisons it doesn't really matter which order we attach the last three
         return dendropy.Tree(taxon_namespace=taxa_metadata.taxon_namespace, seed_node=utils.merge_children((G[i] for i in available_clades)), is_rooted=False)
-def __repr__():
+    def __repr__(self):
         return "SNJ"
+
 
 class SpectralTreeReconstruction(ReconstructionMethod):
     def __init__(self, inner_method, similarity_metric):
@@ -644,8 +748,8 @@ class SpectralTreeReconstruction(ReconstructionMethod):
             
     def __call__(self, sequences, taxa_metadata=None):
         return self.deep_spectral_tree_reconstruction(sequences, self.similarity_metric, taxa_metadata=taxa_metadata, reconstruction_alg = self.inner_method)
-    def __repr__():
-        return "spectralTree"
+    def __repr__(self):
+        return "spectralTree" + " + " + self.inner_method.__repr__(self.inner_method)
 
     def deep_spectral_tree_reconstruction(self, sequences, similarity_metric, taxa_metadata = None, num_gaps =1,threshhold = 100, min_split = 1,merge_method = "angle", verbose = False, **kargs):
         self.verbose = verbose
@@ -662,7 +766,9 @@ class SpectralTreeReconstruction(ReconstructionMethod):
 
         partitioning_tree = MyTree([True]*len(self.taxon_namespace))
         cur_node = partitioning_tree.root
+        #process = psutil.Process(os.getpid())
         while True:
+            #print(process.memory_info().rss/1024, "KB")  # in bytes    
             if (cur_node.right != None) and (cur_node.right.tree !=None) and (cur_node.left.tree != None):
                 cur_node.tree = self.margeTreesLeftRight(cur_node, merge_method)
                 cur_node.tree.mask = np.logical_or(cur_node.right.tree.mask, cur_node.left.tree.mask)
