94ec840c197d542b26379b88cf54dc86f04df69e
Yariv A <aizeny@gmail.com>
2020-07-31 10:22:26 -0400

    adding bin_forrwst method

--- spectraltree/reconstruct_tree.py
+++ spectraltree/reconstruct_tree.py
@@ -693,6 +693,19 @@ class CLRG(ReconstructionMethod):
     def __repr__(self):
         return "CLRG"
 
+class Forrest(ReconstructionMethod):
+    
+    def __call__(self, observations, taxa_metadata=None):        
+        return self.estimate_tree_topology(observations, taxa_metadata)
+    def estimate_tree_topology(self, observations, taxa_metadata=None,bifurcating=False):
+        octave.addpath('./experiments/ltt-1.4/')
+        oc = oct2py.Oct2Py()
+        num_taxa = observations.shape[0]
+        adj_mat = oc.feval("./experiments/ltt-1.4/bin_forrest_wrapper.m",observations+1,4)
+        tree_Forrest = utils.adjacency_matrix_to_tree(adj_mat,num_taxa,taxa_metadata)
+        return tree_Forrest
+    def __repr__(self):
+        return "Forrest"
 
 class DistanceReconstructionMethod(ReconstructionMethod):
     def __init__(self, similarity_metric):
@@ -797,9 +810,9 @@ class SpectralTreeReconstruction(ReconstructionMethod):
 
         partitioning_tree = MyTree([True]*len(self.taxon_namespace))
         cur_node = partitioning_tree.root
-        #process = psutil.Process(os.getpid())
+        if self.verbose: process = psutil.Process(os.getpid())
         while True:
-            #print(process.memory_info().rss/1024, "KB")  # in bytes    
+            if self.verbose: print(process.memory_info().rss/1024, "KB")  # in bytes    
             if (cur_node.right != None) and (cur_node.right.tree !=None) and (cur_node.left.tree != None):
                 cur_node.tree = self.margeTreesLeftRight(cur_node, merge_method)
                 cur_node.tree.mask = np.logical_or(cur_node.right.tree.mask, cur_node.left.tree.mask)
