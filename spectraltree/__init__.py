190e4c133ad7700ca201d2ba0a57e1a57819cb38
Noah Amsel <ebminmaj75@gmail.com>
2021-03-07 16:21:29 -0500

    some renaming

--- spectraltree/__init__.py
+++ spectraltree/__init__.py
@@ -40,7 +40,7 @@ from .spectral_tree_reconstruction import (
     correlation_distance_matrix,
     SpectralTreeReconstruction)
 
-from .str import STR
+from .recursive_str import STR
 from .raxml_reconstruction import RAxML
 from .choi_reconstruction import RG, CLRG
 from .forrest_reconstruction import Forrest
